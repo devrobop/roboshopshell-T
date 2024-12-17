82f77a56c6d51a77f7193b3726b94b27adc412d1
prudhvi-1122 <prudhwi1998@gmail.com>
2024-12-17 20:38:17 +0530

    route 53 updated

---
+++ redish.sh
@@ -0,0 +1,15 @@
+dnf module disable redis -y
+dnf module enable redis:7 -y
+
+dnf install redis -y 
+
+# Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/redis/redis.conf
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf
+
+# Update protected-mode from yes to no in /etc/redis/redis.conf
+
+sed -i 's/protected-mode yes/protected-mode no/g' /etc/redis/redis.conf
+
+systemctl enable redis 
+systemctl start redis 
